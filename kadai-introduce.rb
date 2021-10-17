4215cb9e490518af6f436c40f8cb01297eeddd6e
nanriehub <d50deko@yahoo.co.jp>
2021-10-17 08:40:34 +0000

    third kadai5

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,10 +1,7 @@
-introhosoku= [55.to_s,"歳","です。"]
-myoji = "南部"
+myouji = "南部"
 namae = "利江子"
-
-
-puts myoji + namae + introhosoku[2] + introhosoku[0] + introhosoku[1] + introhosoku[2]
-
+age = 55
+puts myouji + namae + "です。" + 55.to_s + "歳です。"
 
 
 
